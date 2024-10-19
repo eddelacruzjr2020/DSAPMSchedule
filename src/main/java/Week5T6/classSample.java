e86533221829abebed4b29a58a27d752806e436f
Ed Dela Cruz <edmundo.delacruz@cartrack.com>
2024-10-19 16:53:18 +0800

    Week 5 to 6 Codes

---
+++ src/main/java/Week5T6/classSample.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Week5T6;
+
+/**
+ *
+ * @author Edmundo Dela Cruz
+ */
+public class classSample {
+    
+   public void main(String[] args){
+       stackExample1 se = new stackExample1();
+   }
+}
